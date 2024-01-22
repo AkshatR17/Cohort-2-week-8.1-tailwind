3fe6b42a87faae5ff6629a28c3cd45025e8f135c
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-22 22:24:31 +0530

    revenue card done intially

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import "./App.css";
+import { RevenueCard } from "./components/RevenueCard";
 
 function App() {
   return (
     <>
-      {/* <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ background: "red", width: "30%" }}> Red </div>
-        <div style={{ background: "green", width: "30%" }}> Green </div>
-        <div style={{ background: "pink", width: "30%" }}> Pink </div>
-      </div> */}
-     {/* below is tailwind class plus we can't write class like in html as class is reserved word in jsx so we use className instead */}
-      <div className="grid grid-cols-1 mx-2 text-center md:grid-cols-5">
-        <div className="bg-red-500  mx-1 my-1 md:col-span-2"> Red </div>
-        <div className="bg-green-500 mx-1 my-1 md:col-span-2"> Green </div>
-        <div className="bg-pink-500 mx-1 my-1 md:col-span-1"> Pink </div>
-      </div>
-    
+    <div className="grid grid-cols-4">
+      <RevenueCard title={"Amount pending"} orderCount={13} amount= {"92,312.20"}></RevenueCard>
+    </div>
     </>
   );
 }
---
+++ src/components/RevenueCard.jsx
@@ -0,0 +1,52 @@
+// eslint-disable-next-line react/prop-types
+export const RevenueCard = ({ title, orderCount, amount }) => {
+  return (
+    <div className="bg-white rounded shadow-md p-4">
+      <div className="text-gray-700 flex">
+        <div>{title}</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-5 h-5 mt-1 ml-1"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+          />
+        </svg>
+      </div>
+
+      <div className="flex justify-between pt-2">
+        <div className="font-semibold text-2xl">₹ {amount}</div>
+        <div>
+          {orderCount ? (
+            <div className="flex text-blue-700 cursor-pointer underline font-medium flex-col justify-center">
+              <div className="flex">
+                {" "}
+                <div className="">{orderCount} Orders</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-5 h-5 mt-1 stroke-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
