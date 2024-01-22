fea5b86e2aa18e8df1d583e5679ab159af16c249
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-22 23:10:40 +0530

    Storybook setup done

---
+++ src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,13 @@
+import { RevenueCard } from '../../components/RevenueCard';
+
+export default {
+  component: RevenueCard,
+};
+
+export const Primary = {
+  args: {
+    title: "Amount pending",
+    orderCount: 13,
+    amount: "92,312.20"
+  },
+};
